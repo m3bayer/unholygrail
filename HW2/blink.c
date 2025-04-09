d92dbbce3377fd84615e5865eb389f72c14b7be7
m3bayer <m3bayer@gmail.com>
2025-04-09 16:15:40 -0500

    hw2 update/ hw3

--- HW2/blink.c
+++ HW2/blink.c
@@ -1,21 +1,14 @@
-/**
- * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
- *
- * SPDX-License-Identifier: BSD-3-Clause
- */
-
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-// Pico W devices use a GPIO on the WIFI chip for the LED,
-// so when building for Pico W, CYW43_WL_GPIO_LED_PIN will be defined
+
 #ifdef CYW43_WL_GPIO_LED_PIN
 #include "pico/cyw43_arch.h"
 #endif
 
 
-#define LED_PIN 16      // Change if you're using an external LED
-#define BUTTON_PIN 17   // Change to your actual button GPIO
+#define LED_PIN 16
+#define BUTTON_PIN 17
 
 int main() {
     stdio_init_all();
@@ -25,7 +18,7 @@ int main() {
 
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_up(BUTTON_PIN); // Use internal pull-up (active-low button)
+    gpio_pull_up(BUTTON_PIN);
 
     bool led_on = false;
     bool prev_button_state = true;
@@ -34,7 +27,7 @@ int main() {
     while (true) {
         bool button_state = gpio_get(BUTTON_PIN);
 
-        // Check if button was just pressed
+
         if (!button_state && prev_button_state) {
             led_on = !led_on;
             gpio_put(LED_PIN, led_on);
